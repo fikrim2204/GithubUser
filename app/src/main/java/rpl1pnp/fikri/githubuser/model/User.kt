a6b02acc80a7b746fac0de42a30ef269c50e1cab
fikrimaulana <maulsana25@gmail.com>
2021-03-15 12:14:59 +0700

    Add Detail Activity

--- app/src/main/java/rpl1pnp/fikri/githubuser/model/User.kt
+++ app/src/main/java/rpl1pnp/fikri/githubuser/model/User.kt
@@ -4,4 +4,20 @@ import android.os.Parcelable
 import kotlinx.parcelize.Parcelize
 
 @Parcelize
-data class User(val username: String, val name: String, val img: Int) : Parcelable
+data class User(
+    val username: String,
+    val name: String,
+    val img: Int,
+    val follower: String,
+    val following: String,
+    val company: String,
+    val location: String,
+    val repository: List<Repository>
+) : Parcelable
+
+@Parcelize
+data class Repository(
+    val title: String,
+    val description: String,
+    val language: String
+) : Parcelable
---
+++ app/src/main/java/rpl1pnp/fikri/githubuser/view/DetailActivity.kt
@@ -0,0 +1,12 @@
+package rpl1pnp.fikri.githubuser.view
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import rpl1pnp.fikri.githubuser.R
+
+class DetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail)
+    }
+}
