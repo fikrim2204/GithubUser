7bc5f638ea4fee52125b0cfb51486ec7f507da64
fikrimaulana <maulsana25@gmail.com>
2021-03-16 15:27:46 +0700

    work in Main activity and Detail Activity

--- app/src/main/java/rpl1pnp/fikri/githubuser/view/DetailActivity.kt
+++ app/src/main/java/rpl1pnp/fikri/githubuser/view/DetailActivity.kt
@@ -2,11 +2,35 @@ package rpl1pnp.fikri.githubuser.view
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import rpl1pnp.fikri.githubuser.R
+import coil.load
+import rpl1pnp.fikri.githubuser.databinding.ActivityDetailBinding
+import rpl1pnp.fikri.githubuser.model.User
 
 class DetailActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityDetailBinding
+    private var user: User? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_detail)
+        binding = ActivityDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        user = intent.getParcelableExtra(USER)
+        initUser()
+    }
+
+    private fun initUser() {
+        user?.let { binding.civProfile.load(it.img) }
+        binding.tvNameProfile.text = user?.name
+        binding.tvUsernameProfile.text = user?.username
+        binding.tvNumberFollower.text = user?.follower.toString()
+        binding.tvNumberFollowing.text = user?.following.toString()
+        binding.tvLocation.text = user?.location
+        binding.tvCompany.text = user?.company
+        binding.tvNumberRepository.text = user?.repository.toString()
+    }
+
+    companion object {
+        private const val USER = "user"
     }
 }
--- app/src/main/java/rpl1pnp/fikri/githubuser/view/MainActivity.kt
+++ app/src/main/java/rpl1pnp/fikri/githubuser/view/MainActivity.kt
@@ -1,12 +1,37 @@
 package rpl1pnp.fikri.githubuser.view
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import rpl1pnp.fikri.githubuser.R
+import androidx.recyclerview.widget.LinearLayoutManager
+import rpl1pnp.fikri.githubuser.adapter.MainAdapter
+import rpl1pnp.fikri.githubuser.databinding.ActivityMainBinding
+import rpl1pnp.fikri.githubuser.model.User
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var adapter: MainAdapter
+    private var user: MutableList<User> = mutableListOf()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.rvUserGithub.layoutManager = LinearLayoutManager(this)
+        initUser()
+        adapter = MainAdapter(user) {
+            val intent = Intent(this, DetailActivity::class.java)
+            intent.putExtra(USER, it)
+            startActivity(intent)
+        }
+    }
+
+    private fun initUser() {
+        TODO("Not yet implemented")
+    }
+
+    companion object {
+        private const val USER = "user"
     }
 }
