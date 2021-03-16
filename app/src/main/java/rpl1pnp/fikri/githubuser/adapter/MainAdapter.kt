2ac29e6a111c275dff84d759ed39fff8393ce4f9
fikrimaulana <maulsana25@gmail.com>
2021-03-16 14:59:13 +0700

    coding main adapter

---
+++ app/src/main/java/rpl1pnp/fikri/githubuser/adapter/MainAdapter.kt
@@ -0,0 +1,37 @@
+package rpl1pnp.fikri.githubuser.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import coil.load
+import rpl1pnp.fikri.githubuser.adapter.MainAdapter.ViewHolder
+import rpl1pnp.fikri.githubuser.databinding.ItemMainBinding
+import rpl1pnp.fikri.githubuser.model.User
+
+class MainAdapter(private var item: List<User>, private val listener: (User) -> Unit) :
+    RecyclerView.Adapter<ViewHolder>() {
+    class ViewHolder(private val binding: ItemMainBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(user: User, listener: (User) -> Unit) {
+            binding.civProfile.load(user.img)
+            binding.tvNameProfile.text = user.name
+            binding.tvUsernameProfile.text = user.username
+            binding.tvFollower.text = user.follower.toString()
+            binding.tvFollowing.text = user.following.toString()
+            itemView.setOnClickListener {
+                listener(user)
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        val binding = ItemMainBinding.inflate(layoutInflater)
+        return ViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(item[position], listener)
+    }
+
+    override fun getItemCount(): Int = item.size
+}
