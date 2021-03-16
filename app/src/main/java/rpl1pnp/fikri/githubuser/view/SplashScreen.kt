ca96f735dff44ef90d15a2a88512b6f483a9ee97
fikrimaulana <maulsana25@gmail.com>
2021-03-16 12:47:11 +0700

    create splash screen code

--- app/src/main/java/rpl1pnp/fikri/githubuser/view/SplashScreen.kt
+++ app/src/main/java/rpl1pnp/fikri/githubuser/view/SplashScreen.kt
@@ -1,6 +1,8 @@
 package rpl1pnp.fikri.githubuser.view
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
 import androidx.appcompat.app.AppCompatActivity
 import rpl1pnp.fikri.githubuser.R
 
@@ -8,5 +10,10 @@ class SplashScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        Handler(mainLooper).postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, 5000)
     }
 }
