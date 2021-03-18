865837ce2b10682c5f1a5071c924c64f09d823c3
fikrimaulana <maulsana25@gmail.com>
2021-03-18 15:19:03 +0700

    design item card

---
+++ app/src/main/java/rpl1pnp/fikri/githubuser/view/fragment/DetailFragment.kt
@@ -0,0 +1,22 @@
+package rpl1pnp.fikri.githubuser.view.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import rpl1pnp.fikri.githubuser.R
+class DetailFragment : Fragment() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_detail, container, false)
+    }
+
+}
